97ea1504e62417665ee2459a51fa13e7d1093945
Dinesh Kumaraguru <dinesh.k@payoda.com>
2019-01-30 11:46:02 +0530

    Added List matches page with API data.

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -7,40 +7,15 @@ import {MatchServiceService} from './services/match-service.service'
   styleUrls: ['list.page.scss']
 })
 export class ListPage implements OnInit {
-  private selectedItem: any;
-  private icons = [
-    'flask',
-    'wifi',
-    'beer',
-    'football',
-    'basketball',
-    'paper-plane',
-    'american-football',
-    'boat',
-    'bluetooth',
-    'build'
-  ];
   public items= [];
   result={};
+  today:Date = new Date();
   constructor(private MatchServiceService:MatchServiceService) {    
-    // for (let i = 1; i < 11; i++) {
-    //   this.items.push({
-    //     title: 'Item ' + i,
-    //     note: 'This is item #' + i,
-    //     icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-    //   });
-    // }
-  }
+   }
   
   ngOnInit() {
     this.MatchServiceService.getCricket().subscribe((result) => {
         this.items = result.json().content;
-        console.log("this.items",result.json());
     });
   }
-  
-  // add back when alpha.4 is out
-  // navigate(item) {
-  //   this.router.navigate(['/list', JSON.stringify(item)]);
-  // }
 }
